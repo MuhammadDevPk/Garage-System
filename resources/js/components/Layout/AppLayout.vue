27332770110d4847d91af03399fc1636bcc0838d
Malik Muhammad Awan <muhammad.dev.pk@gmail.com>
2026-03-02 11:19:15 +0500

    initial

---
+++ resources/js/components/Layout/AppLayout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="flex h-screen bg-gray-100">
+        <sidebar />
+
+        <div class="flex flex-1 flex-col">
+            <Topbar />
+            <main class="p-6 overflow-y-auto">
+                <slot />
+            </main>
+        </div>
+    </div>
+</template>
